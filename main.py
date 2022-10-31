53178f391f9896e9ad48f438b769e31c38044174
ABHax101 <72935354+ABHax101@users.noreply.github.com>
2022-10-31 15:37:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ st.title('Stock Forecast App')
 stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
-n_years = st.slider('Years of prediction:', 1, 4)
+n_years = st.slider('Years of prediction:', 1, 10)
 period = n_years * 365
 
 
